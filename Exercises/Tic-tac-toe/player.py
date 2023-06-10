7126f2dfd378b5a7e37515ab368bacbc1e99a1ac
LoyumM <lmbmoirangthem033@gmail.com>
2023-06-10 23:28:07 +0530

    tictactoe

---
+++ Exercises/Tic-tac-toe/player.py
@@ -0,0 +1,32 @@
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        #letter is x or o
+        self.letter = letter
+        
+    #we want all players to ger their next move given a name
+    def get_move(self, game):
+        pass
+    
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+    
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + "\'s turn. Input move (0-9):")
+            # we're going to check that this is a correcct input by trying to cast
+            # it to an integer and if it's not, then we say its invalid
+            # If that spot in not available on the board, we also say its invalid
