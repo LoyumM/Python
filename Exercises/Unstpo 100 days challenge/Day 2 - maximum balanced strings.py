1d349d8d096432f304e3a62187ecab50be4a9f98
LoyumM <lmbmoirangthem033@gmail.com>
2023-10-08 22:06:43 +0530

    alternative solution

--- Exercises/Unstpo 100 days challenge/Day 2 - maximum balanced strings.py
+++ Exercises/Unstpo 100 days challenge/Day 2 - maximum balanced strings.py
@@ -19,3 +19,31 @@ def balanced_string(s):
 input_string = input()
 output = balanced_string(input_string)
 print(output)
+
+
+
+#### chatGPT solution:
+
+def balanced_string_split(s):
+    result = []
+    count = 0
+    current_balanced = ""
+
+    for char in s:
+        if char == 'L':
+            count += 1
+        else:
+            count -= 1
+        
+        current_balanced += char
+
+        if count == 0:
+            result.append(current_balanced)
+            current_balanced = ""
+
+    return "(" + ")(".join(result) + ")"
+
+# Test case
+input_str = "RLRRLLRLRL"
+output_str = balanced_string_split(input_str)
+print(output_str)
