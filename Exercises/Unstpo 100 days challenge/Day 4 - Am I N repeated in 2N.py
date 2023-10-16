90fdc353e67f0aa3fba34a20dcac1af3472f1c3d
LoyumM <lmbmoirangthem033@gmail.com>
2023-10-16 23:10:50 +0530

    more day 4 questions

---
+++ Exercises/Unstpo 100 days challenge/Day 4 - Am I N repeated in 2N.py
@@ -0,0 +1,21 @@
+def repeated_2n(numbers:list, k:int):
+    element_counts = {}
+
+    for element in numbers:
+        if element in element_counts:
+            element_counts[element] += 1
+        else:
+            element_counts[element] = 1
+
+    target_count = k // 2
+    result_element = None
+
+    for element, count in element_counts.items():
+        if count == target_count:
+            result_element = element
+            break
+    return result_element
+
+k = int(input())
+numbers = list(map(int, input().split()))
+print(repeated_2n(numbers, k))
