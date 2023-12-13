80dda8098faa42e7bf5e45ae6e082f471f012679
LoyumM <lmbmoirangthem033@gmail.com>
2023-12-13 17:53:55 +0530

    different solution

---
+++ Exercises/Data Lemur/Two sum.py
@@ -0,0 +1,32 @@
+#Brute-Force: O(n^2)
+def two_sum(input: list[int], target: int):
+    for i in range(len(input)-1):
+        for j in range(i+1,len(input)):
+            if input[i] + input[j] == target:
+                return [i,j]
+    return [-1, -1]
+
+lst = [1, 4, 6, 10] 
+target = 10
+
+print(two_sum(lst, target))
+
+dictionary: O(n)
+
+def two_sum(input: list[int], target) -> list[int]:
+    d = {}
+    for i in range(len(input)):
+
+        cur_value = input[i]
+        complement = target - cur_value
+        if complement in d:
+            return [d[complement], i]
+        d[cur_value] = i
+        print(d)
+
+    return [-1, -1]
+
+lst = [1, 4, 6, 10] 
+target = 10
+
+print(two_sum(lst, target))
---
+++ Exercises/Data Lemur/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+ #     complement = target - cur_value
+    #     if complement in d:
+    #         return [d[complement], i]
+    #     d[cur_value] = i
+
+    # return [-1, -1]
