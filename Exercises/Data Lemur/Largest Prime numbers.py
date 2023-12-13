9bbdeae845480da27059dbe0d809b1fe6c9d480c
LoyumM <lmbmoirangthem033@gmail.com>
2023-12-13 18:47:11 +0530

    updated solution

--- Exercises/Data Lemur/Largest Prime numbers.py
+++ Exercises/Data Lemur/Largest Prime numbers.py
@@ -13,3 +13,22 @@ def largest_prime_factor(target):
         if is_prime and j > 1:
             prime_factors.append(j)
     return max(list(set(prime_factors)))
+
+
+
+def largest_prime_factor(target):
+    max_prime_factor = 1
+
+    while target % 2 == 0:
+        max_prime_factor = 2
+        target //= 2
+
+    for i in range(3, int(target**0.5) + 1, 2):
+        while target % i == 0:
+            max_prime_factor = i
+            target //= i
+
+    if target > 2:
+        max_prime_factor = target
+
+    return max_prime_factor
