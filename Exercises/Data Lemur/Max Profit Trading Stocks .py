b2b2f707903d64c32af2f53f335fa66b5f471e58
LoyumM <lmbmoirangthem033@gmail.com>
2023-12-13 17:41:37 +0530

    alternate solution

--- Exercises/Data Lemur/Max Profit Trading Stocks .py
+++ Exercises/Data Lemur/Max Profit Trading Stocks .py
@@ -1,3 +1,4 @@
+#Brute-Force Solution: O(n)^2 complexity
 def max_subarray_sum(prices):
     diff = 0
     for i in range(len(prices)):
@@ -7,3 +8,13 @@ def max_subarray_sum(prices):
     return diff
             
 print(max_subarray_sum([9, 1, 3, 6, 4, 8, 3, 5, 5]))
+
+# a solution with O(n) complexity
+def max_profit(prices):
+    min_price = prices[0]
+    max_profit = 0
+    for cur_price in prices[1:]:
+        max_profit = max(max_profit, cur_price - min_price)
+        min_price = min(min_price, cur_price)
+    
+    return max_profit
