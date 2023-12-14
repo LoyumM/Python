f8f93439b30516c3c49dfe8934a3cbbd40fd1e95
LoyumM <lmbmoirangthem033@gmail.com>
2023-12-14 15:15:03 +0530

    smallest multiple

---
+++ Exercises/Data Lemur/Smallest number.py
@@ -0,0 +1,13 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def smallest_multiple(target):
+    result = 1
+    for i in range(1, target + 1):
+        result = lcm(result, i)
+    return result
