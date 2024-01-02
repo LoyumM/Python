027d4b5f66dd42e5f994452425de10d08a0ff39e
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-02 15:15:01 +0530

    max profit

---
+++ Leetcode/Best Time to Buy and Sell Stock Submission Detail.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        max_profit = 0
+        for cur_price in prices[1:]:
+            max_profit = max(max_profit, cur_price - min_price)
+            min_price = min(min_price, cur_price)        
+        return max_profit
