df24a8ed9af1fdfe3700253e495a853aa68e2140
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-09 23:27:04 +0530

    less complex

--- Leetcode/152. Maximum Product Subarray.py
+++ Leetcode/152. Maximum Product Subarray.py
@@ -1,4 +1,4 @@
-#time complexity O(n)2
+#time complexity O(n2)
 def max_product(arr:list):
     n = len(arr)
     max_product_val = float('-inf')
@@ -14,3 +14,17 @@ def max_product(arr:list):
 
 nums = [2, 3, -2, 4]
 print(max_product(nums))
+
+#time complexity O(n)
+def maxProduct(nums:list):
+    res = max(nums)
+
+    curMax, curMin = 1,1
+
+    for num in nums:
+        tmp = curMax * num
+        curMax = max(curMax * num, curMin * num, num)
+        curMin = min(tmp, curMin * num, num)
+        res = max(res, curMax)
+
+    return res
