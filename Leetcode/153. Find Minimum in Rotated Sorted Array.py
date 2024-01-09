13b497ade557008d4b6faaf310a329c24be2c18b
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-10 00:15:24 +0530

    binary search minimum

--- Leetcode/153. Find Minimum in Rotated Sorted Array.py
+++ Leetcode/153. Find Minimum in Rotated Sorted Array.py
@@ -14,3 +14,27 @@ nums = [3,4,5,1,2]
 sol_instance = Solution()
 res = sol_instance.findMin(nums)
 print(res)
+
+
+# time complexity O(logn)
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        res = nums[0]
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            if nums[left] < nums[right]:
+                res = min(res, nums[left])
+                break
+
+            center = (left + right) // 2
+            res = min(res, nums[center])
+
+            if nums[center] >= nums[left]:    
+                left = center + 1
+
+            else:
+                right = center - 1
+
+        return res
+            
