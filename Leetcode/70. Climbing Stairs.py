a5c1dd9f04fb6c9d8f18b60a82355afb9b48f07d
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-15 15:38:33 +0530

    dp solution

--- Leetcode/70. Climbing Stairs.py
+++ Leetcode/70. Climbing Stairs.py
@@ -1,17 +1,15 @@
-import matplotlib.pyplot as plt
+# brute force
+def climbStairs(n: int):
+    if n == 0 or n == 1:
+        return 1
 
-#brute force
-# def climbStairs(n: int):
-#     if n == 0 or n == 1:
-#         return 1
+    ways = [0] * (n + 1)
+    ways[0], ways[1] = 1, 1
 
-#     ways = [0] * (n + 1)
-#     ways[0], ways[1] = 1, 1
+    for i in range(2, n + 1):
+        ways[i] = ways[i - 1] + ways[i - 2]
 
-#     for i in range(2, n + 1):
-#         ways[i] = ways[i - 1] + ways[i - 2]
-
-#     return ways[n]
+    return ways[n]
 
 # brute force using recursion
 def climbStairs(n:int):
@@ -20,19 +18,31 @@ def climbStairs(n:int):
     else:
         return climbStairs(n - 1) + climbStairs(n - 2)
 
+# dynamic programming 
+class Solution:
+    def climbStairs(self, n:int):
+        one, two = 1, 1
+        
+        for _ in range(n-1):
+            temp = one 
+            one = one + two
+            two = temp
+        return
+    
 # Example usage
+solution = Solution()
 stairs = []
-for i in range(0, 30):
+for i in range(0, 40):
     n = i
-    output = climbStairs(n)
+    output = solution.climbStairs(n)
     print(f"Output for n={n}: {output}")
     stairs.append(output)
-    
+
+import matplotlib.pyplot as plt
 # Plotting i on the y-axis and the corresponding number of ways on the x-axis
-plt.plot(range(30), stairs, marker='o', linestyle='-', color='blue')
+plt.plot(range(40), stairs, marker='o', linestyle='-', color='blue')
 plt.title('Number of Ways to Climb Stairs')
 plt.xlabel('Target number')
 plt.ylabel('Number of Ways')
 plt.grid(True)
-plt.show()
-    
+# plt.show()
