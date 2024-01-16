18f7a19bc735eee1d71de87023d20f82791b126d
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-16 16:12:40 +0530

    coming back to you later

--- Leetcode/322. Coin Change.py
+++ Leetcode/322. Coin Change.py
@@ -1,29 +1,34 @@
 # obvious solution
-
 class Solution:
     def coinChange(self, coins: list[int], amount: int):
         coins = sorted(coins)
         counter = 0
         target = 0
         idx = -1
-        while target < amount:
+        for index in range(-1, -(len(coins)), -1):
             try:
-                if target + coins[idx] < amount:
-                    target += coins[idx]
-                    counter += 1
-                elif target + coins[idx] > amount:
-                    idx -= 1
-                elif target + coins[idx] == amount:
-                    counter += 1
-                    return counter    
+                while target < amount:
+                    if target + coins[idx] < amount:
+                        target += coins[idx]
+                        counter += 1
+                        print(f"target: {target}, counter: {counter}")
+                    elif target + coins[idx] > amount:
+                        idx -= 1
+                        print(f"target: {target}, counter: {counter}, idx: {idx}")
+                    elif target + coins[idx] == amount:
+                        counter += 1
+                        print(f"target: {target}, counter: {counter}")
+                        return counter    
             except IndexError:
-                return -1
-        target = 0
-        while target + min(coins) > amount:
-            return 0
+                counter = 0
+                target = 0
+                idx = index
+        # target = 0
+        # while target + min(coins) > amount:
+        #     return 0
 
-coins = [1,2,3]
-amount = 0
+coins = [1,3,4,5]
+amount = 7
 solution = Solution()
 res = solution.coinChange(coins, amount)
 print(res)
