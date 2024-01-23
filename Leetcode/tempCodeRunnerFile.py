42e3583c902a25aa25cb0035e9669903fdaf2646
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-23 18:14:52 +0530

    removing unccessary file

--- Leetcode/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-start, end = 0, 0
-for end in range(len(nums)):
-    if nums[end] == val:
-        end += 1
-    else: #nums[end] != val:
-        nums[start],nums[end] = nums[end],nums[start]
-        start += 1
-        end += 1 
