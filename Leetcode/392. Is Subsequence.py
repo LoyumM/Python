9d9ee0409c8afef4b8746084fa338e561156c95f
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-24 16:33:02 +0530

    two working solutions

--- Leetcode/392. Is Subsequence.py
+++ Leetcode/392. Is Subsequence.py
@@ -1,11 +1,11 @@
 # Decent enough two pointer solution
-# def issubsequence(s, t):
-#     idx1, idx2 = 0, 0
-#     while idx2 < len(t):
-#         if idx1 < len(s) and t[idx2] == s[idx1]:
-#             idx1 += 1
-#         idx2 += 1
-#     return idx1 == len(s)
+def issubsequence(s, t):
+    idx1, idx2 = 0, 0
+    while idx2 < len(t):
+        if idx1 < len(s) and t[idx2] == s[idx1]:
+            idx1 += 1
+        idx2 += 1
+    return idx1 == len(s)
 
 # but we can see that idx2 moves every loop regardless, so we can only use one and get the same result
 # this uses more memory however
