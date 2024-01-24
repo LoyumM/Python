834ca27d1bbfe7740800451ec08ed8ab4a72eb2e
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-24 16:19:13 +0530

    first logic

---
+++ Leetcode/392. Is Subsequence.py
@@ -0,0 +1,14 @@
+# First attempt: Doesnt pass all test cases
+def issubsequence(s, t):
+    idx1, idx2 = 0, 0
+    while idx2 < len(t):
+        if idx1 < len(s) and t[idx2] == s[idx1]:
+            idx1 += 1
+        idx2 += 1
+    return idx1 == len(s)
+
+
+s = "abc"
+t = "ahbgdc"
+# s , t = "axc", "ahbgdc"
+print(issubsequence(s,t))
