406fd95773488c47fc440780eb9fd9df23f7548c
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-24 14:38:25 +0530

    hasmap

--- Leetcode/219. Contains Duplicate II.py
+++ Leetcode/219. Contains Duplicate II.py
@@ -8,3 +8,18 @@ def containsNearbyDuplicate(nums: list, k: int):
 nums = [1,2,3,1,2,3]
 k = 2
 print(containsNearbyDuplicate(nums, k))
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        d1 = {}
+        for key, val in enumerate(nums):
+            if val not in d1:
+                d1[val] = key
+            else:
+                if abs(d1.get(val) - key) <= k:
+                    return True
+                else:
+                    d1[val] = key
+
+        return False
