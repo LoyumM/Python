1256a1d22f4cf325de955b901abff9f65ec7fe93
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-25 14:00:32 +0530

    multiple solutions

--- Leetcode/80. Remove Duplicates from Sorted Array II.py
+++ Leetcode/80. Remove Duplicates from Sorted Array II.py
@@ -1,17 +1,29 @@
-def removeDuplicates(nums: list[int]):
-    k = 0
-    for idx, val in enumerate(nums):
-        print(f"new loop {nums}")
-        if k < 2 or val != nums[k - 2]:
-            print(f"during the loop {nums}")
-            print(f"k {k}, idx {idx}, nums[k - 2] {nums[k - 2]}, val {val}")
-            nums[k] = val
-            print(f"swapped {nums}")
-            k += 1
-            print(f"k {k}, idx {idx}, nums[k - 2] {nums[k - 2]}, val {val}")
-    print(nums)
-    return k 
+# neetcode solution
+class Solution:
+        def removeDuplicates(self, nums: list[int]):
+            l, r = 0, 0
+            while r < len(nums):
+                count = 1
+                while r+1 < len(nums) and nums[r] == nums[r+1]:
+                    r += 1
+                    count += 1
+                for i in range(min(2, count)):
+                    nums[l] = nums[r]
+                    l += 1
+                r += 1
+            return l
+
+
+# other people's solution
+class Solution:
+        def removeDuplicates(self, nums: list[int]):
+            k = 0
+            for val in nums:
+                if k < 2 or val != nums[k - 2]:
+                    nums[k] = val
+                    k += 1
+            return k 
 
 # nums = [1,1,1,2,2,3]
 nums = [0,0,1,1,1,1,2,3,3]
-print(removeDuplicates(nums))
+# print(removeDuplicates(nums))
