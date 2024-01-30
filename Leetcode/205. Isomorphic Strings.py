7eafa3ec3f674bb544d046e685e20d69191b2a12
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-30 16:11:03 +0530

    solution without zip

--- Leetcode/205. Isomorphic Strings.py
+++ Leetcode/205. Isomorphic Strings.py
@@ -20,28 +20,53 @@
 # print(d1 == d2)
 
 
-
-def isIsomorphic(s, t):
+def isIsomorphic(s: list, t: list):
     if len(s) != len(t):
         return False
+    
+    dict1 = {}
+    dict2 = {}
 
-    mapping_s_to_t = {}
-    mapping_t_to_s = {}
-
-    for char_s, char_t in zip(s, t):
-        if char_s in mapping_s_to_t:
-            if mapping_s_to_t[char_s] != char_t:
+    for idx in range(len(s)):
+        chr_s, chr_t = s[idx], t[idx]
+        
+        if chr_s in dict1:
+            if dict1[chr_s] != chr_t:
+                return False       
+        else:
+            dict1[chr_s] = chr_t
+        
+        if chr_t in dict2:
+            if dict2[chr_t] != chr_s:
                 return False
         else:
-            mapping_s_to_t[char_s] = char_t
+            dict2[chr_t] = chr_s
 
-        if char_t in mapping_t_to_s:
-            if mapping_t_to_s[char_t] != char_s:
-                return False
-        else:
-            mapping_t_to_s[char_t] = char_s
-    print(mapping_s_to_t, mapping_t_to_s)
-    return True
+    return True           
+
+
+
+# def isIsomorphic(s, t):
+#     if len(s) != len(t):
+#         return False
+
+#     mapping_s_to_t = {}
+#     mapping_t_to_s = {}
+
+#     for char_s, char_t in zip(s, t):
+#         if char_s in mapping_s_to_t:
+#             if mapping_s_to_t[char_s] != char_t:
+#                 return False
+#         else:
+#             mapping_s_to_t[char_s] = char_t
+
+#         if char_t in mapping_t_to_s:
+#             if mapping_t_to_s[char_t] != char_s:
+#                 return False
+#         else:
+#             mapping_t_to_s[char_t] = char_s
+#     print(mapping_s_to_t, mapping_t_to_s)
+#     return True
 
 
 # Example usage:
