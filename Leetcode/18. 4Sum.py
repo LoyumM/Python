d1b4a52685c52c040df20e0b68f71cf50acc33bc
LoyumM <lmbmoirangthem033@gmail.com>
2024-02-06 12:15:31 +0530

    general solution for 3 sum and above

--- Leetcode/18. 4Sum.py
+++ Leetcode/18. 4Sum.py
@@ -16,3 +16,45 @@ class Solution:
                         l+=1
                         r-=1
         return list(s)
+    
+# a more general approach
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        def twoSum(nums, target):
+            lo, hi = 0, len(nums) - 1
+            res = []
+            while lo < hi:
+                total = nums[lo] + nums[hi]
+                if total < target:
+                    lo += 1
+                elif total > target:
+                    hi -= 1
+                else:
+                    res.append([nums[lo], nums[hi]])
+                    lo += 1
+                    hi -= 1
+                    while lo < hi and nums[lo] == nums[lo-1]: lo += 1
+                    while lo < hi and nums[hi] == nums[hi+1]: hi -= 1
+            return res
+                    
+        def kSum(nums, target, k):
+            if not nums:
+                return []
+            
+            avg = target // k
+            if nums[0] > avg or nums[-1] < avg:
+                return []
+        
+            if k == 2:
+                return twoSum(nums, target)
+            
+            res = []
+            for i in range(len(nums)):
+                if i > 0 and nums[i] == nums[i-1]: continue
+                for subset in kSum(nums[i+1:], target - nums[i], k - 1):
+                    res.append([nums[i]] + subset)
+            
+            return res
+            
+        nums.sort()
+        return kSum(nums, target, 4)
