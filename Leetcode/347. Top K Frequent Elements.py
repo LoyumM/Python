612a0826f9bb09fe6ad73a50bb2e1ec016cb0433
LoyumM <lmbmoirangthem033@gmail.com>
2025-12-26 11:50:52 +0530

    updated solution

--- Leetcode/347. Top K Frequent Elements.py
+++ Leetcode/347. Top K Frequent Elements.py
@@ -15,7 +15,6 @@ class Solution:
 
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        # Step 1: Count the frequency of each element manually
         frequency = {}
         for num in nums:
             if num in frequency:
@@ -23,18 +22,13 @@ class Solution:
             else:
                 frequency[num] = 1
 
-        # Step 2: Create an array of unique elements
         unique_elements = list(frequency.keys())
         
-        # Step 3: Sort the unique elements based on their frequency manually
-        # Here, we will use a simple selection sort to sort the frequencies
         for i in range(len(unique_elements)):
             max_idx = i
             for j in range(i + 1, len(unique_elements)):
                 if frequency[unique_elements[j]] > frequency[unique_elements[max_idx]]:
                     max_idx = j
-            # Swap the elements to sort by frequency
             unique_elements[i], unique_elements[max_idx] = unique_elements[max_idx], unique_elements[i]
 
-        # Step 4: Return the top k frequent elements
         return unique_elements[:k]
